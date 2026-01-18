e86f2115abca34a0bd5b7afa5eaa2e430631bef0
natively.dev <api@natively.dev>
2026-01-18 00:42:22 +0000

    fix: Correct ESLint configuration to resolve build/lint failure

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,7 @@ module.exports = {
     'expo',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime'
+    'plugin:react/recommended'
   ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'react', 'import'],
