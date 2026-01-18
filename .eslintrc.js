51aa7b852a1e26e6948dcca39bf520d8fbbd3e8a
natively.dev <api@natively.dev>
2026-01-18 00:27:16 +0000

    fix: Remove invalid react-native env setting from ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,7 +34,8 @@ module.exports = {
   ],
   env: {
     browser: true,
-    'react-native/react-native': true,
+    node: true,
+    es6: true
   },
   rules: {
     "@typescript-eslint/no-unused-vars": "off",
