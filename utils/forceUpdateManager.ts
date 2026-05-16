1201962d898d68085e0a7416de5df46462e71cff
natively.dev <api@natively.dev>
2026-05-16 17:25:00 +0000

    build: Bump version to 2.3.0 and prepare for TestFlight

--- utils/forceUpdateManager.ts
+++ utils/forceUpdateManager.ts
@@ -1,6 +1,6 @@
 import Constants from 'expo-constants';
 
-const MINIMUM_VERSION = '2.2.0';
+const MINIMUM_VERSION = '2.3.0';
 const APP_STORE_URL = 'https://apps.apple.com/app/id6744042838';
 
 export type VersionCheckResult =
